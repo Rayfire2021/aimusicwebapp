864aca410d1e8182364a1aceb74c666c6dc08504
Rayfire2021 <93227458+Rayfire2021@users.noreply.github.com>
2023-10-20 19:36:35 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+song = "";
+
+function preload()
+{
+    song = loadSound("backontrack.mp3");
+    song = loadSound("dryout")
+}
+
+function setup() {
+    canvas = createCanvas(600, 500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+}
+
+function modelLoaded() {
+    console.log('PoseNet Is Initialized');
+}
+
+function draw() {
+    image(video, 0, 0, 600, 500);
+}
+
+function play()
+{
+    song.play();
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = " + leftWristX +" leftWristY ="+ leftWristY);
+
+       rightWristX = results[0].pose.rightWrist.x;
+       rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = " + rightWristX +" rightWristY ="+ rightWristY);
+    }
+    }
